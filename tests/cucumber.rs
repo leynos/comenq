b70fa9766aadd6bd0ef137d64aa66632a1fa8f51
Leynos <leynos@troubledskies.net>
2025-08-01 21:35:39 +0100

    Automate release with GoReleaser (#28)
    * Document Linux-only release
    
    * Update release workflow and tests
    
    * Update release workflow and promote serde_yaml
    
    * More liberal tagging pattern

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -8,13 +8,15 @@ mod support;
 mod util;
 use cucumber::World as _;
 use steps::{
-    CliWorld, ClientWorld, CommentWorld, ConfigWorld, ListenerWorld, PackagingWorld, WorkerWorld,
+    CliWorld, ClientWorld, CommentWorld, ConfigWorld, ListenerWorld, PackagingWorld, ReleaseWorld,
+    WorkerWorld,
 };
 
 #[tokio::main]
 async fn main() {
     tokio::join!(
         CliWorld::run("tests/features/cli.feature"),
+        ReleaseWorld::run("tests/features/release.feature"),
         ClientWorld::run("tests/features/client_main.feature"),
         CommentWorld::run("tests/features/comment_request.feature"),
         ConfigWorld::run("tests/features/config.feature"),
